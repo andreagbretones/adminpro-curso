b997b580072725fc1352d42b3bff7ea6fae6d1de
Andrea García <andrea.garcia@quantion.com>
2021-02-01 14:04:46 +0100

    cambios en el routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+
+//Rutas
 import { APP_ROUTES } from './app-routes';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { PagesComponent } from './pages/pages.component';
 
-
+//Modulos
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
-  declarations: [AppComponent, NopagefoundComponent,SidebarComponent,HeaderComponent, PagesComponent],
-  imports: [BrowserModule, APP_ROUTES],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, APP_ROUTES, PagesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
