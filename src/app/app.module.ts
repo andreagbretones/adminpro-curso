1527a88e5ebbaf9891a2c845a3995676c295104a
Andrea García <andrea.garcia@quantion.com>
2021-02-01 12:34:12 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { APP_ROUTES } from './app-routes';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PagesComponent } from './pages/pages.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NopagefoundComponent,SidebarComponent,HeaderComponent, PagesComponent],
+  imports: [BrowserModule, APP_ROUTES],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
